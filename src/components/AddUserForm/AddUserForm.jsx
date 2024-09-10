3409b66721e6b1e06ae22937d4110c6a84678ac4
Tetiana Koval <tetiana.koval.bit@gmail.com>
2024-09-10 19:50:31 +0300

    added country and status inputs with lists

--- src/components/AddUserForm/AddUserForm.jsx
+++ src/components/AddUserForm/AddUserForm.jsx
@@ -8,9 +8,9 @@ export const AddUserForm = ({ setShowAddUserForm }) => {
   const formRef = useRef(null);
   const [name, setName] = useState("");
   const [department, setDepartment] = useState(["Select department", false]);
-  const [country, setCountry] = useState("");
-  const [status, setStatus] = useState("");
-  const [listDepOpen, setListDepOpen] = useState(false);
+  const [country, setCountry] = useState(["Select country", false]);
+  const [status, setStatus] = useState(["Select status", false]);
+  const [listOpen, setListOpen] = useState([false, false, false]);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -34,6 +34,28 @@ export const AddUserForm = ({ setShowAddUserForm }) => {
     };
   }, [setShowAddUserForm]);
 
+  const hendleSelectItem = (category, selectedItem) => {
+
+    switch (category) {
+      case 'departments':
+        setDepartment([selectedItem, true]);
+        break;
+
+      case 'countries':
+        setCountry([selectedItem, true]);
+        break;
+
+      case 'statuses':
+        setStatus([selectedItem, true]);
+        break;
+
+      default:
+        console.log("error");
+    }
+
+    setListOpen([false, false, false]);
+  }
+
   return (
     <div className="container-bg">
       <div className="add-user-form" ref={formRef}>
@@ -53,37 +75,126 @@ export const AddUserForm = ({ setShowAddUserForm }) => {
               />
             </div>
 
-            <div className="form__input">
+            <div
+              className="form__input"
+              onMouseLeave={() => setListOpen([false, false, false])}
+            >
               <label htmlFor="department">Department:</label>
 
               <div
                 className={cn("form__input--input", {
                   selected: department[1],
-                  "form__input--input-active": listDepOpen,
+                  "form__input--input-active": listOpen[0],
                 })}
                 onClick={() => {
-                  setListDepOpen(!listDepOpen);
+                  setListOpen([!listOpen[0], listOpen[1], listOpen[2]]);
                 }}
               >
                 {department}
                 <span className={cn({
-                  'arrow-active': listDepOpen,
+                  'arrow-active': listOpen[0],
                 })}></span>
               </div>
 
-              {listDepOpen && (
-                <ul className="form__input-list">
+              {listOpen[0] && (
+                <ul
+                  className="form__input-list"
+                >
                   <CustomScrollbars style={{ width: 280, height: 100 }}>
                     {departments.map((dep) => (
                       <li
                         key={dep.value}
                         onClick={() => {
-                          setDepartment([dep.name, true]);
-                          setListDepOpen(false);
+                          hendleSelectItem('departments', dep.name)
                         }}
                         className="form__input-item"
                       >
                         {dep.name}
+                      </li>
+                    ))}
+                  </CustomScrollbars>
+                </ul>
+              )}
+            </div>
+
+            <div
+              className="form__input"
+              onMouseLeave={() => setListOpen([false, false, false])}
+            >
+              <label htmlFor="department">Country:</label>
+
+              <div
+                className={cn("form__input--input", {
+                  selected: country[1],
+                  "form__input--input-active": listOpen[1],
+                })}
+                onClick={() => {
+                  setListOpen([listOpen[0], !listOpen[1], listOpen[2]]);
+                }}
+              >
+                {country}
+                <span className={cn({
+                  'arrow-active': listOpen[1],
+                })}></span>
+              </div>
+
+              {listOpen[1] && (
+                <ul
+                  className="form__input-list"
+                >
+                  <CustomScrollbars style={{ width: 280, height: 100 }}>
+                    {countries.map((coun) => (
+                      <li
+                        key={coun.value}
+                        onClick={() => {
+                          hendleSelectItem('countries', coun.name)
+                        }}
+                        className="form__input-item"
+                      >
+                        {coun.name}
+                      </li>
+                    ))}
+                  </CustomScrollbars>
+                </ul>
+              )}
+            </div>
+
+            <div
+              className="form__input"
+              onMouseLeave={() => setListOpen([false, false, false])}
+            >
+              <label htmlFor="department">Status:</label>
+
+              <div
+                className={cn("form__input--input", {
+                  selected: status[1],
+                  "form__input--input-active": listOpen[2],
+                })}
+                onClick={() => {
+                  setListOpen([listOpen[0], listOpen[1], !listOpen[2]]);
+                }}
+              >
+                {status}
+                <span className={cn({
+                  'arrow-active': listOpen[2],
+                })}></span>
+              </div>
+
+              {listOpen[2] && (
+                <ul
+                  className="form__input-list"
+                  
+                >
+                  <CustomScrollbars style={{ width: 280, height: 100 }}>
+                    {statuses.map((stat) => (
+                      <li
+                        key={stat.value}
+                        onClick={() => {
+                          hendleSelectItem('statuses', stat.name)
+                        }}
+                        className="form__input-item"
+                      >
+                        {stat.name}
                       </li>
                     ))}
                   </CustomScrollbars>
