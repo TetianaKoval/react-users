61bc3718385651f8c0250a33d3602a48c61c53c0
Tetiana Koval <tetiana.koval.bit@gmail.com>
2024-09-14 17:24:34 +0300

    data storage of the edited user is implemented

--- src/components/EditUsers/EditUsers.jsx
+++ src/components/EditUsers/EditUsers.jsx
@@ -14,7 +14,7 @@ export const EditUsers = () => {
   const [selectedDepartment, setSelectedDepartment] = useState(null);
   const [selectedCountry, setSelectedCountry] = useState(null);
   const [selectedStatus, setSelectedStatus] = useState(null);
-  const { users } = useContext(UsersContext);
+  const { users, setUsers } = useContext(UsersContext);
   const [listUsersNamesOpen, setListUsersNamesOpen] = useState(false);
   const [listDepartmentsOpen, setListDepartmentsOpen] = useState(false);
   const [listCountriesOpen, setListCountriesOpen] = useState(false);
@@ -32,7 +32,7 @@ export const EditUsers = () => {
   };
 
   const handleDepartmentSelect = (dep) => {
-    setSelectedDepartment(dep.name);
+    setSelectedDepartment([dep.name, dep.value]);
     setListDepartmentsOpen(false);
 
     if (dep.name === selectedUser.department.name) {
@@ -41,7 +41,7 @@ export const EditUsers = () => {
   };
 
   const handleCountrySelect = (coun) => {
-    setSelectedCountry(coun.name);
+    setSelectedCountry([coun.name, coun.value]);
     setListCountriesOpen(false);
 
     if (coun.name === selectedUser.country.name) {
@@ -50,7 +50,7 @@ export const EditUsers = () => {
   };
 
   const handleStatusSelect = (stat) => {
-    setSelectedStatus(stat.name);
+    setSelectedStatus([stat.name, stat.value]);
     setListStatusesOpen(false);
 
     if (stat.name === selectedUser.status.name) {
@@ -68,6 +68,40 @@ export const EditUsers = () => {
     setName(newName);
   };
 
+  const saveEditUserInfo = () => {
+    const newUsers = [...users].map(user => {
+      if (user.name === selectedUser.name) {
+        return {
+          name: name,
+          status: {
+            name: selectedStatus ? selectedStatus[0] : user.status.name,
+            value: selectedStatus ? selectedStatus[1] : user.status.value,
+          },
+          department: {
+            name: selectedDepartment ? selectedDepartment[0] : user.department.name,
+            value: selectedDepartment ? selectedDepartment[1] : user.department.value,
+          },
+          country: {
+            name: selectedCountry ? selectedCountry[0] : user.country.name,
+            value: selectedCountry ? selectedCountry[1] : user.country.value,
+          }
+        }
+      } else {
+        return user;
+      }
+    })
+
+    setUsers([...newUsers]);
+
+    setSelectedUser(null)
+
+    setSelectedDepartment(null);
+    setSelectedCountry(null);
+    setSelectedStatus(null);
+    setName('');
+    setEdiedName(false);
+  }
+
   useEffect(() => {
     if (selectedDepartment || selectedCountry || selectedStatus || ediedName) {
       setAddUndo(true);
@@ -81,7 +115,7 @@ export const EditUsers = () => {
       <h1 className="edit-user__title title">Edit user</h1>
 
       <div
-        className="form__input edit-user__input"
+        className="form__input edit-user__input edit-user__input--users"
         onMouseLeave={() => setListUsersNamesOpen(false)}
       >
         <label htmlFor="name">User</label>
@@ -118,132 +152,135 @@ export const EditUsers = () => {
       </div>
 
       {selectedUser && (
-        <div className="edit-user__form">
-          <div className="form__input edit-user__input">
-            <label htmlFor="name">Full name</label>
+        <div>
+          <div className="edit-user__sub-title">User Information</div>
+          <div className="edit-user__form">
+            <div className="form__input edit-user__input">
+              <label htmlFor="name">Full name</label>
 
-            <input
-              className={cn("form__input--input", {
-                selected: ediedName,
-              })}
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setNewFullName(e.target.value)}
-              required
-              placeholder={selectedUser.name}
-            />
-          </div>
-
-          <div
-            className="form__input edit-user__input"
-            onMouseLeave={() => setListDepartmentsOpen(false)}
-          >
-            <label htmlFor="department">Department</label>
-            <div
-              className={cn("form__input--input", {
-                selected: selectedDepartment,
-                "form__input--input-active": listDepartmentsOpen,
-              })}
-              onClick={() => setListDepartmentsOpen(!listDepartmentsOpen)}
-            >
-              {selectedDepartment || selectedUser.department.name}
-              <span
-                className={cn({
-                  "arrow-active": listDepartmentsOpen,
+              <input
+                className={cn("form__input--input", {
+                  selected: ediedName,
                 })}
-              ></span>
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setNewFullName(e.target.value)}
+                required
+                placeholder={selectedUser.name}
+              />
             </div>
 
-            {listDepartmentsOpen && (
-              <ul className="form__input-list">
-                <CustomScrollbars style={{ width: 280, height: 100 }}>
-                  {departments.map((dep) => (
-                    <li
-                      key={dep.name}
-                      onClick={() => handleDepartmentSelect(dep)}
-                      className="form__input-item"
-                    >
-                      {dep.name}
-                    </li>
-                  ))}
-                </CustomScrollbars>
-              </ul>
-            )}
-          </div>
-
-          <div
-            className="form__input edit-user__input"
-            onMouseLeave={() => setListCountriesOpen(false)}
-          >
-            <label htmlFor="name">Country</label>
             <div
-              className={cn("form__input--input", {
-                selected: selectedCountry,
-                "form__input--input-active": listCountriesOpen,
-              })}
-              onClick={() => setListCountriesOpen(!listCountriesOpen)}
+              className="form__input edit-user__input"
+              onMouseLeave={() => setListDepartmentsOpen(false)}
             >
-              {selectedCountry || selectedUser.country.name}
-              <span
-                className={cn({
-                  "arrow-active": listCountriesOpen,
+              <label htmlFor="department">Department</label>
+              <div
+                className={cn("form__input--input", {
+                  selected: selectedDepartment,
+                  "form__input--input-active": listDepartmentsOpen,
                 })}
-              ></span>
+                onClick={() => setListDepartmentsOpen(!listDepartmentsOpen)}
+              >
+                { selectedDepartment ? selectedDepartment[0] : selectedUser.department.name }
+                <span
+                  className={cn({
+                    "arrow-active": listDepartmentsOpen,
+                  })}
+                ></span>
+              </div>
+
+              {listDepartmentsOpen && (
+                <ul className="form__input-list">
+                  <CustomScrollbars style={{ width: 280, height: 100 }}>
+                    {departments.map((dep) => (
+                      <li
+                        key={dep.name}
+                        onClick={() => handleDepartmentSelect(dep)}
+                        className="form__input-item"
+                      >
+                        {dep.name}
+                      </li>
+                    ))}
+                  </CustomScrollbars>
+                </ul>
+              )}
             </div>
 
-            {listCountriesOpen && (
-              <ul className="form__input-list">
-                <CustomScrollbars style={{ width: 280, height: 100 }}>
-                  {countries.map((coun) => (
-                    <li
-                      key={coun.name}
-                      onClick={() => handleCountrySelect(coun)}
-                      className="form__input-item"
-                    >
-                      {coun.name}
-                    </li>
-                  ))}
-                </CustomScrollbars>
-              </ul>
-            )}
-          </div>
-
-          <div
-            className="form__input edit-user__input"
-            onMouseLeave={() => setListStatusesOpen(false)}
-          >
-            <label htmlFor="name">Status</label>
             <div
-              className={cn("form__input--input", {
-                selected: selectedStatus,
-                "form__input--input-active": listStatusesOpen,
-              })}
-              onClick={() => setListStatusesOpen(!listStatusesOpen)}
+              className="form__input edit-user__input"
+              onMouseLeave={() => setListCountriesOpen(false)}
             >
-              {selectedStatus || selectedUser.status.name}
-              <span
-                className={cn({
-                  "arrow-active": listStatusesOpen,
+              <label htmlFor="name">Country</label>
+              <div
+                className={cn("form__input--input", {
+                  selected: selectedCountry,
+                  "form__input--input-active": listCountriesOpen,
                 })}
-              ></span>
+                onClick={() => setListCountriesOpen(!listCountriesOpen)}
+              >
+                { selectedCountry ? selectedCountry[0] : selectedUser.country.name}
+                <span
+                  className={cn({
+                    "arrow-active": listCountriesOpen,
+                  })}
+                ></span>
+              </div>
+
+              {listCountriesOpen && (
+                <ul className="form__input-list">
+                  <CustomScrollbars style={{ width: 280, height: 100 }}>
+                    {countries.map((coun) => (
+                      <li
+                        key={coun.name}
+                        onClick={() => handleCountrySelect(coun)}
+                        className="form__input-item"
+                      >
+                        {coun.name}
+                      </li>
+                    ))}
+                  </CustomScrollbars>
+                </ul>
+              )}
             </div>
 
-            {listStatusesOpen && (
-              <ul className="form__input-list">
-                <CustomScrollbars style={{ width: 280, height: 100 }}>
-                  {statuses.map((stat) => (
-                    <li
-                      key={stat.name}
-                      onClick={() => handleStatusSelect(stat)}
-                      className="form__input-item"
-                    >
-                      {stat.name}
-                    </li>
-                  ))}
-                </CustomScrollbars>
-              </ul>
-            )}
+            <div
+              className="form__input edit-user__input"
+              onMouseLeave={() => setListStatusesOpen(false)}
+            >
+              <label htmlFor="name">Status</label>
+              <div
+                className={cn("form__input--input", {
+                  selected: selectedStatus,
+                  "form__input--input-active": listStatusesOpen,
+                })}
+                onClick={() => setListStatusesOpen(!listStatusesOpen)}
+              >
+                {selectedStatus ? selectedStatus[0] : selectedUser.status.name}
+                <span
+                  className={cn({
+                    "arrow-active": listStatusesOpen,
+                  })}
+                ></span>
+              </div>
+
+              {listStatusesOpen && (
+                <ul className="form__input-list">
+                  <CustomScrollbars style={{ width: 280, height: 100 }}>
+                    {statuses.map((stat) => (
+                      <li
+                        key={stat.name}
+                        onClick={() => handleStatusSelect(stat)}
+                        className="form__input-item"
+                      >
+                        {stat.name}
+                      </li>
+                    ))}
+                  </CustomScrollbars>
+                </ul>
+              )}
+            </div>
           </div>
         </div>
       )}
@@ -255,9 +292,17 @@ export const EditUsers = () => {
           </div>
         )}
 
-        <div className="btn btn--undo">
-          Save
-        </div>
+        {selectedUser && (
+          <div
+            className={cn("btn", {
+              'disable': !addUndo,
+            })}
+            onClick={saveEditUserInfo}
+          >
+            Save
+          </div>
+        )}
+        
       </div>
     </div>
   );
